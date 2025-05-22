7a17043ee8bc2b14d2597001ce343fc100ae2a01
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2025-05-22 17:10:55 -0400

    configured open api api key

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+# backend/config.py
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI_API_KEY")
+
+if not api_key:
+    raise ValueError("OPENAI_API_KEY is missing in .env")
